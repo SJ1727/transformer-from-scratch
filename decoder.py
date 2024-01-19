2c011910da1335c9fb2bcd5160d2ab2c4b822c54
SJ1727 <samueljohnson1727@gmail.com>
2024-01-19 12:42:19 +0000

    Added even more documentation

--- decoder.py
+++ decoder.py
@@ -5,8 +5,16 @@ import torch.optim as optim
 from helper import AddAndNorm, MultiHeadAttention, FeedForwarLayer
 
 class Decoder(nn.Module):
-    """Some Information about Decoder"""
     def __init__(self, embed_dim: int, num_heads: int, feed_forward_dim: int, num_layers: int):
+        """
+        Decoder Section of the transformer
+
+        Args:
+            embed_dim (int): The dimension of the word embeddings
+            num_heads (int): The number of heads to be used
+            feed_forward_dim (int): The dimension of the feed forward layers hidden layer
+            num_layers (int): The number of decoder layers
+        """
         super(Decoder, self).__init__()
         
         self.decoder = nn.ModuleList([
@@ -14,20 +22,48 @@ class Decoder(nn.Module):
             for _ in range(num_layers)
         ])
 
-    def forward(self, x, cross_attention_kv):
+    def forward(self, x: torch.tensor, cross_attention_kv: torch.tensor) -> torch.tensor:
+        """
+        Forward pass of the decoder
+
+        Args:
+            x (torch.tensor): Input tensor
+            cross_attention_kv (torch.tensor): Tensor to be used in cross attention
+
+        Returns:
+            torch.tensor: Output tensor
+        """
         for block in self.decoder:
             x = block(x, cross_attention_kv)
         return x
 
 class DecoderBlock(nn.Module):
     def __init__(self, embed_dim: int, num_heads: int, feed_forward_dim: int):
+        """
+        Signular layer of a decoder
+
+        Args:
+            embed_dim (int): Word embedding dimension
+            num_heads (int): Number of heads to be used
+            feed_forward_dim (int): The dimension of the hidden layer in the feed forward layers
+        """
         super(DecoderBlock, self).__init__()
         
         self.self_attention = AddAndNorm(MultiHeadAttention(embed_dim, num_heads=num_heads), embed_dim)
         self.cross_attention = AddAndNorm(MultiHeadAttention(embed_dim, num_heads=num_heads), embed_dim)
         self.feed_forward = AddAndNorm(FeedForwarLayer(embed_dim, feed_forward_dim), embed_dim)
 
-    def forward(self, x, cross_attention_kv):
+    def forward(self, x: torch.tensor, cross_attention_kv: torch.tensor) -> torch.tensor:
+        """
+        Forward pass of the decoder block
+
+        Args:
+            x (torch.tensor): Input tensor
+            cross_attention_kv (torch.tensor): Tesnor to be used in cross attention
+
+        Returns:
+            torch.tensor: Output tensor
+        """
         x = self.self_attention(x, masked=True)
         x = self.cross_attention(x, cross_attention_kv=cross_attention_kv)
         x = self.feed_forward(x)
--- encoder.py
+++ encoder.py
@@ -6,6 +6,15 @@ from helper import AddAndNorm, MultiHeadAttention, FeedForwarLayer
 
 class Encoder(nn.Module):
     def __init__(self, embed_dim: int, num_heads: int, feed_forward_dim: int, num_layers: int):
+        """
+        Encoder section of the transformer
+
+        Args:
+            embed_dim (int): The dimension of the word embeddings
+            num_heads (int): The number of heads to be used
+            feed_forward_dim (int): The dimension of the hidden layer in the feed forward layers
+            num_layers (int): The number of encoder layers to be applied
+        """
         super(Encoder, self).__init__()
 
         self.encoder = nn.ModuleList([
@@ -13,18 +22,44 @@ class Encoder(nn.Module):
             for _ in range(num_layers)
         ])
 
-    def forward(self, x):
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        """
+        Forward pass of the encoder
+
+        Args:
+            x (torch.tensor): Input tensor
+
+        Returns:
+            torch.tensor: Output Tensor
+        """
         for block in self.encoder:
             x = block(x)
         return x
 
 class EncoderBlock(nn.Module):
     def __init__(self, embed_dim: int, num_heads: int, feed_forward_dim: int):
+        """
+        Singular layer of the encoder
+
+        Args:
+            embed_dim (int): The dimension of the word embedding
+            num_heads (int): The number of heads to be used
+            feed_forward_dim (int): The dimension of the hidden layer in the feed forward layers
+        """
         super(EncoderBlock, self).__init__()
         self.self_attention = AddAndNorm(MultiHeadAttention(embed_dim, num_heads=num_heads), embed_dim)
         self.feed_forward = AddAndNorm(FeedForwarLayer(embed_dim, feed_forward_dim), embed_dim)
 
-    def forward(self, x):
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        """
+        Forward pass of the encoder block
+
+        Args:
+            x (torch.tensor): Input tensor
+
+        Returns:
+            torch.tensor: Output tensor
+        """
         x = self.self_attention(x)
         x = self.feed_forward(x)
         return x
