Fixed softmax

transformer.py
@@ -14,7 +14,7 @@ class Transformer(nn.Module):
         self.encoder = Encoder(embed_dim, num_heads, feed_forward_dim, num_layers)
         self.decoder = Decoder(embed_dim, num_heads, feed_forward_dim, num_layers)
         self.linear = nn.Linear(embed_dim, vocabulary_size)
-        self.softmax = F.log_softmax(dim=-1)
+        self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, source: torch.tensor, target: torch.tensor):
         source = self.positional_encodings(source)